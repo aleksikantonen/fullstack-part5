13985c0628b44d4fccdefa94885ef5d8404bac9f
Aleksi Kantonen <aleksi@artnik.fi>
2025-12-21 13:41:01 +0200

    5c-5.14

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders blog title and author by default', () => {
@@ -32,5 +33,27 @@ test('does not render URL or likes by default', () => {
 
   const likes = screen.queryByText('likes 5')
   expect(likes).toBeNull()
+})
+
+test('renders URL and likes when view button is clicked', async () => {
+  const blog = {
+    title: 'Test Blog Title',
+    author: 'Test Author',
+    url: 'https://test.com',
+    likes: 5,
+    id: '123'
+  }
+
+  render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton)
+
+  const url = screen.getByText('https://test.com')
+  expect(url).toBeDefined()
+
+  const likes = screen.getByText('likes 5')
+  expect(likes).toBeDefined()
 })
 
