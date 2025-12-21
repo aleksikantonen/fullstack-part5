c3767f874fe8473553c85508e434d318dccce382
Aleksi Kantonen <aleksi@artnik.fi>
2025-12-21 13:46:51 +0200

    refactor: remove duplications with mutual reset state in tests

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import { vi } from 'vitest'
 import Blog from './Blog'
 
-test('renders blog title and author by default', () => {
+describe('<Blog />', () => {
   const blog = {
     title: 'Test Blog Title',
     author: 'Test Author',
@@ -11,49 +12,49 @@ test('renders blog title and author by default', () => {
     id: '123'
   }
 
-  render(<Blog blog={blog} />)
+  beforeEach(() => {
+    render(<Blog blog={blog} />)
+  })
 
-  const titleAuthor = screen.getByText('Test Blog Title Test Author')
-  expect(titleAuthor).toBeDefined()
-})
+  test('renders blog title and author by default', () => {
+    const titleAuthor = screen.getByText('Test Blog Title Test Author')
+    expect(titleAuthor).toBeDefined()
+  })
 
-test('does not render URL or likes by default', () => {
-  const blog = {
-    title: 'Test Blog Title',
-    author: 'Test Author',
-    url: 'https://test.com',
-    likes: 5,
-    id: '123'
-  }
+  test('does not render URL or likes by default', () => {
+    const url = screen.queryByText('https://test.com')
+    expect(url).toBeNull()
 
-  render(<Blog blog={blog} />)
+    const likes = screen.queryByText('likes 5')
+    expect(likes).toBeNull()
+  })
 
-  const url = screen.queryByText('https://test.com')
-  expect(url).toBeNull()
+  test('renders URL and likes when view button is clicked', async () => {
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
 
-  const likes = screen.queryByText('likes 5')
-  expect(likes).toBeNull()
-})
+    const url = screen.getByText('https://test.com')
+    expect(url).toBeDefined()
 
-test('renders URL and likes when view button is clicked', async () => {
-  const blog = {
-    title: 'Test Blog Title',
-    author: 'Test Author',
-    url: 'https://test.com',
-    likes: 5,
-    id: '123'
-  }
+    const likes = screen.getByText('likes 5')
+    expect(likes).toBeDefined()
+  })
 
-  render(<Blog blog={blog} />)
+  test('clicking like button twice calls event handler twice', async () => {
+    const mockUpdateBlog = vi.fn()
 
-  const user = userEvent.setup()
-  const viewButton = screen.getByText('view')
-  await user.click(viewButton)
+    render(<Blog blog={blog} updateBlog={mockUpdateBlog} />)
 
-  const url = screen.getByText('https://test.com')
-  expect(url).toBeDefined()
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
 
-  const likes = screen.getByText('likes 5')
-  expect(likes).toBeDefined()
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mockUpdateBlog.mock.calls).toHaveLength(2)
+  })
 })
 
